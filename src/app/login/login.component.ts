4fa1238495a858ad602ccc89986c1f3e7858779c
Sahil pillania <sahilpillania88@gmail.com>
2023-06-18 10:42:40 +0530

    login func completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  username: string= "";
+  password: string= "";
+  errorMsg: string= "";
+  constructor( private auth: AuthService, private router: Router){
+    
+
+  
+  }
+
+  ngOnInit(): void {}
+
+  login(){
+    if(this.username.trim().length === 0) {
+      this.errorMsg = "Username is required";
+  
+  }else if(this.password.trim().length === 0) {
+    this.errorMsg = "password is required";
+
+}else {
+  this.errorMsg = "";
+  let res = this.auth.login(this.username, this.password);
+  if(res === 200){
+    this.router.navigate(['home'])
+  
+  }else{
+  this.errorMsg = "Check the fields again";
+  }
+}
+
+}
 
 }
